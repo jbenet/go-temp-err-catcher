c531232018e678b2a702cfb86b5c3f68d1c8beb8
Juan Batiz-Benet <juan@benet.ai>
2015-01-11 12:33:18 -0800

    streaks and starts

--- tec_test.go
+++ tec_test.go
@@ -65,6 +65,100 @@ func TestTemporary(t *testing.T) {
 	})
 }
 
+func TestDoubles(t *testing.T) {
+	last := time.Now()
+	diff := func() time.Duration {
+		now := time.Now()
+		diff := now.Sub(last)
+		last = now
+		return diff
+	}
+
+	testDiff := func(low, hi time.Duration) {
+		d := diff()
+		grace := time.Duration(time.Microsecond)
+		if (d + grace) < low {
+			t.Error("time difference is smaller than", low, d)
+		}
+		if (d - grace) > hi {
+			t.Error("time difference is greater than", hi, d)
+		}
+	}
+
+	var c TempErrCatcher
+	testDiff(0, c.Start)
+	c.IsTemporary(ErrTemp)
+	testDiff(c.Start, 2*c.Start) // first time.
+	c.IsTemporary(ErrTemp)
+	testDiff(2*c.Start, 4*c.Start) // second time.
+	c.IsTemporary(ErrTemp)
+	testDiff(4*c.Start, 8*c.Start) // third time.
+}
+
+func TestDifferentStart(t *testing.T) {
+	last := time.Now()
+	diff := func() time.Duration {
+		now := time.Now()
+		diff := now.Sub(last)
+		last = now
+		return diff
+	}
+
+	testDiff := func(low, hi time.Duration) {
+		d := diff()
+		grace := time.Duration(time.Microsecond)
+		if (d + grace) < low {
+			t.Error("time difference is smaller than", low, d)
+		}
+		if (d - grace) > hi {
+			t.Error("time difference is greater than", hi, d)
+		}
+	}
+
+	var c TempErrCatcher
+	f := time.Millisecond
+	testDiff(0, f)
+	c.IsTemporary(ErrTemp)
+	testDiff(f, 2*f) // first time.
+	c.IsTemporary(ErrTemp)
+	testDiff(2*f, 4*f) // second time.
+	c.IsTemporary(ErrTemp)
+	testDiff(4*f, 8*f) // third time.
+
+	c.Reset()
+	c.Start = 10 * time.Millisecond
+	f = c.Start
+	testDiff(0, f)
+	c.IsTemporary(ErrTemp)
+	testDiff(f, 2*f) // first time.
+	c.IsTemporary(ErrTemp)
+	testDiff(2*f, 4*f) // second time.
+	c.IsTemporary(ErrTemp)
+	testDiff(4*f, 8*f) // third time.
+}
+
+func TestDifferentStreaks(t *testing.T) {
+	var c TempErrCatcher
+	// one streak
+	c.IsTemporary(ErrTemp) // 1
+	c.IsTemporary(ErrTemp) // 2
+	c.IsTemporary(ErrTemp) // 4
+	expect := 4 * time.Millisecond
+	if c.delay != expect {
+		t.Error("delay should be:", expect, c.delay)
+	}
+
+	<-time.After(c.delay * 10)
+
+	// a different streak
+	c.IsTemporary(ErrTemp) // 1
+	c.IsTemporary(ErrTemp) // 2
+	c.IsTemporary(ErrTemp) // 4
+	if c.delay != expect {
+		t.Error("delay should be:", expect, c.delay)
+	}
+}
+
 func TestFunc(t *testing.T) {
 	var c TempErrCatcher
 	c.IsTemp = func(e error) bool {
--- temp_err_catcher.go
+++ temp_err_catcher.go
@@ -6,6 +6,13 @@ import (
 	"time"
 )
 
+// InitialDelay governs how long to wait the first time.
+// This is defaulted to time.Millisecond, which makes sense
+// for network listener failures. You may want a much smaller
+// delay. You can configure this package wide, or in each
+// TempErrCatcher
+var InitialDelay = time.Millisecond
+
 // Temporary is an interface errors can implement to
 // ensure they are correctly classified by the default
 // TempErrCatcher classifier
@@ -35,7 +42,9 @@ type TempErrCatcher struct {
 	IsTemp func(error) bool    // the classifier to use. default: ErrIsTemporary
 	Wait   func(time.Duration) // the wait func to call. default: time.Sleep
 	Max    time.Duration       // the maximum time to wait. default: time.Second
+	Start  time.Duration       // the delay to start with. default: InitialDelay
 	delay  time.Duration
+	last   time.Time
 }
 
 func (tec *TempErrCatcher) init() {
@@ -48,24 +57,47 @@ func (tec *TempErrCatcher) init() {
 	if tec.Wait == nil {
 		tec.Wait = time.Sleep
 	}
+	if tec.Start == 0 {
+		tec.Start = InitialDelay
+	}
 }
 
-// IsTemporary wa
+// IsTemporary checks whether an error is temporary. It will call
+// tec.Wait before returning, with a delay. The delay is also
+// doubled, so we do not constantly spin. This is the strategy
+// net.Listener uses.
+//
+// Note: you will want to call Reset() if you get a success,
+// so that the stored delay is brough back to 0.
 func (tec *TempErrCatcher) IsTemporary(e error) bool {
 	tec.init()
 	if tec.IsTemp(e) {
-		if tec.delay == 0 {
-			tec.delay = 5 * time.Millisecond
+		now := time.Now()
+		if now.Sub(tec.last) > (tec.delay * 5) {
+			// this is a "new streak" of temp failures. reset.
+			tec.Reset()
+		}
+
+		if tec.delay == 0 { // init case.
+			tec.delay = tec.Start
 		} else {
 			tec.delay *= 2
 		}
+
 		if tec.delay > tec.Max {
 			tec.delay = tec.Max
 		}
 		tec.Wait(tec.delay)
+		tec.last = now
 		return true
 	}
+	tec.Reset() // different failure. call reset
 	return false
+}
+
+// Reset sets the internal delay counter to 0
+func (tec *TempErrCatcher) Reset() {
+	tec.delay = 0
 }
 
 // ErrTemporary wraps any error and implements Temporary function.
